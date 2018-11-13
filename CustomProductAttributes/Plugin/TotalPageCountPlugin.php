0d61342752ef57541aa5a53bf37c97f5fa750c7b
Yuriy Bodak <yuriybodak@MacBook-Pro-Yuriy.local>
2018-11-13 13:45:13 +0200

    Add modules

---
+++ CustomProductAttributes/Plugin/TotalPageCountPlugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yuriybodak
+ * Date: 13.11.18
+ * Time: 11:58
+ */
+
+namespace Training\CustomProductAttributes\Plugin;
+
+
+class TotalPageCountPlugin
+{
+
+}
